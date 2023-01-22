3f9d1368ea221ca3372f37d7e27163d6f3d8e085
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-01-22 13:02:33 -0500

    files

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pydantic import EmailStr
 from fastapi import FastAPI
 from fastapi import status
 from fastapi import Body
-from fastapi import Query, Path, Form, Header, Cookie
+from fastapi import Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -180,3 +180,19 @@ def contact(
     ads: Optional[str]=Cookie(default=None)    
 ):
     return user_agent
+
+
+#Files
+
+@app.post(
+    path="/post-image"
+)
+
+def post_image(
+    image:UploadFile=File(...)
+):
+    return {
+        "Filename":image.filename,
+        "Format":image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024,ndigits=2)
+    }
